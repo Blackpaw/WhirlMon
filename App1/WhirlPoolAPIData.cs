a0024cb548972c22c40579e1825fc128b38d5aa4
Lindsay Mathieson <lindsay.mathieson@gmail.com>
2015-08-30 21:55:28 +1000

    Fancy data template and Prettydate

--- App1/WhirlPoolAPIData.cs
+++ App1/WhirlPoolAPIData.cs
@@ -7,6 +7,59 @@ using System.Threading.Tasks;
 
 namespace WhirlMon
 {
+    public class PrettyDate
+    {
+        const int SECOND = 1;
+        const int MINUTE = 60 * SECOND;
+        const int HOUR = 60 * MINUTE;
+        const int DAY = 24 * HOUR;
+        const int MONTH = 30 * DAY;
+
+        static public String Get(DateTime yourDate)
+        {
+            var ts = new TimeSpan(DateTime.Now.Ticks - yourDate.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 1 * MINUTE)
+            {
+                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
+            }
+            if (delta < 2 * MINUTE)
+            {
+                return "a minute ago";
+            }
+            if (delta < 45 * MINUTE)
+            {
+                return ts.Minutes + " minutes ago";
+            }
+            if (delta < 90 * MINUTE)
+            {
+                return "an hour ago";
+            }
+            if (delta < 24 * HOUR)
+            {
+                return ts.Hours + " hours ago";
+            }
+            if (delta < 48 * HOUR)
+            {
+                return "yesterday";
+            }
+            if (delta < 30 * DAY)
+            {
+                return ts.Days + " days ago";
+            }
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "one month ago" : months + " months ago";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "one year ago" : years + " years ago";
+            }
+        }
+    }
     public class WhirlPoolAPIData
     {
         public class BASE
@@ -48,6 +101,16 @@ namespace WhirlMon
             public int LASTPAGE { get; set; }
             public int REPLIES { get; set; }
             public string FIRST_DATE { get; set; }
+
+            public String LAST_DATE_D
+            {
+                
+                get
+                {
+                    DateTime d = DateTime.Parse(LAST_DATE);
+                    return PrettyDate.Get(d) + " " + d.ToString("h:mm");
+                }
+            }
         }
 
         public class FIRST2
